aaea21a9ea653ef51b8abbdf13add7e341bbac64
SewonK <cutecat1128@naver.com>
2022-03-30 22:04:05 +0900

    [동시성] Hashtable vs Hashmap vs ConcurrentHashmap 내부 로직 확인용

---
+++ src/HashMapPractice.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class HashMapPractice {
+    public static void main(String[] args) {
+        Hashtable<String, Integer> hashtable = new Hashtable<>();
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        ConcurrentHashMap<String, Integer> concurrentHashMap = new ConcurrentHashMap<>();
+
+    }
+}
